e9b668fb2b121019304ada7c901a7280c752788c
bruno31830 <boissiebruno@gmail.com>
2023-05-25 16:51:35 +0200

    Keycloack integration

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,10 +2,12 @@ import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from './AuthContext';
 import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
+import extractName from './utils/textUtils';
 
 const Header = () => {
   const { keycloak } = useContext(AuthContext);
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [name,setName]=useState();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -21,6 +23,18 @@ const Header = () => {
     keycloak.logout();
   };
 
+
+  useEffect(()=>{
+    if (!keycloak.authenticated) return;
+    if (keycloak.tokenParsed.name) {
+      setName(extractName(keycloak.tokenParsed.name));
+    } else if (keycloak.tokenParsed.given_name) {
+      setName(extractName(keycloak.tokenParsed.given_name));
+    } else if (keycloak.tokenParsed.preferred_username) {
+      setName(extractName(keycloak.tokenParsed.preferred_username));
+    }
+  },[keycloak])
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -33,7 +47,7 @@ const Header = () => {
               {currentTime.toLocaleTimeString()}
             </Typography>
             <Typography variant="body2" style={{ marginRight: '10px' }}>
-              Utilisateur Connecté
+             {name}
             </Typography>
             <IconButton color="inherit" onClick={handleLogout}>
               <LogoutIcon />
